5ee06f49f9e7c2f14b9ed1044d9bfa0ec6366372
presli04 <preslavmilchev232@gmail.com>
2026-01-20 13:42:36 +0200

    Added a "rewards" system
    Tried to add a rewards system it works but needs some logical improvement.

--- guess_a_number.py.py
+++ guess_a_number.py.py
@@ -4,6 +4,7 @@ pc_number=random.randint(1,100)
 difficulty=input("Choose difficulty (eg. easy, medium, hard): ")
 tries=0
 counter_of_failed_attemptss=0
+rewards=0
 while True:
     players_input=input("Guess the number between 1 and 100: ")
     if difficulty=="easy":
@@ -16,12 +17,27 @@ while True:
         print("Invalid input,retry!")
         continue
     player_number=int(players_input)
+    if difficulty=="easy" and player_number==pc_number:
+        rewards=10
+        print(f"You gueessed the number on {difficulty} and your reward is {rewards} BGN.Congrats")
+        break
+    elif difficulty=="medium" and player_number==pc_number:
+        rewards=20
+        print(f"You guessed the number om {difficulty} and your reward is {rewards} BGN.Congrats")
+        break
+    elif difficulty=="hard" and player_number==pc_number:
+        rewards=50
+        print(f"You guessed the number on {difficulty} and your reward is {rewards} BGN.Congrats")
+        break
     if player_number!=pc_number:
         counter_of_failed_attemptss+=1
     if counter_of_failed_attemptss>=tries:
         print("Failed to guess the number.")
         print("Do you want to play again?")
         player_answer=input()
+        if not players_input.isdigit():
+            print("Invalid input,retry!")
+            continue
         if player_answer=="yes":
             tries+=1
         else:
